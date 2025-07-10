b2ddbad8748ba1290035a5744e430fd852e79183
jagruthip2007 <jagruthipulumati@Jagruthis-MacBook-Pro.local>
2025-07-10 14:54:20 +0530

    yay

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,11 +3,14 @@ export default function HeroSection() {
     <section className="relative h-[60vh] md:h-[90vh] w-full overflow-hidden flex items-center justify-center text-center">
       <video
         className="absolute top-0 left-0 w-full h-full object-cover brightness-50 z-0"
-        src="https://files.catbox.moe/keqh25.mp4"
+        src="https://files.catbox.moe/ry9mkd.mp4"
         autoPlay
         loop
         muted
         playsInline
+        onCanPlay={(e) => {
+          e.target.playbackRate = 0.75;
+        }}
       />
       <div className="absolute inset-0 bg-[#e7b2c6]/40  z-10" />
       <div className="relative z-20 px-2 md:px-4">
